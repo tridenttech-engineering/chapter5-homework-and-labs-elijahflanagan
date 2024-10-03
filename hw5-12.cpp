a669e59fc442c6c7705a49b0b7ec993b81ed3f20
elijahflanagan <elijahflanagan@my.tridenttech.edu>
2024-10-02 20:47:49 -0400

    Update hw5-12.cpp

--- hw5-12.cpp
+++ hw5-12.cpp
@@ -5,8 +5,25 @@
 #include <iomanip>
 using namespace std;
 
-int main()
-{
+int main() {
+    double price1, price2, total;
 
-	return 0;
-}	//end of main function
+    // Input prices
+    cout << "Enter the price of the first item: ";
+    cin >> price1;
+    cout << "Enter the price of the second item: ";
+    cin >> price2;
+
+    // Calculate total price
+    if (price1 <= price2) {
+        total = price1 + (price2 * 0.5);
+    } else {
+        total = (price1 * 0.5) + price2;
+    }
+
+    // Display the total price in fixed-point notation with two decimals
+    cout << fixed << setprecision(2);
+    cout << "Total amount owed: $" << total << endl;
+
+    return 0;
+}
